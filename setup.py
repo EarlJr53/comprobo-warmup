416caf003dd4366076ef1888ae2e8ad2ff5a8944
Brooke Moss <me@brookem.dev>
2023-09-08 14:00:35 -0400

    Adds drive_square

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'warmup_project'
+package_name = "warmup_project"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='brookem',
-    maintainer_email='me@brookem.dev',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="brookem",
+    maintainer_email="me@brookem.dev",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'teleop = warmup_project.teleop:main'
+        "console_scripts": [
+            "teleop = warmup_project.teleop:main",
+            "drive_square = warmup_project.drive_square:main",
         ],
     },
 )
--- warmup_project/drive_square.py
+++ warmup_project/drive_square.py
@@ -1,9 +1,43 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import math
+from time import sleep
 
 class DriveSquareNode(Node):
     def __init__(self):
         super().__init__('drive_square_node')
+        self.vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
+        # self.odom_sub = self.create_subscription(Odometry, "odom", 10)
+        self.run_loop()
+
+
+    def run_loop(self):
+        for _ in range(4):
+            self.drive_forward(0.5)
+            self.turn_left()
+
+    def turn_left(self):
+        angular_vel = 0.3
+        self.drive(linear=0.0, angular=angular_vel)
+        sleep(math.pi / angular_vel / 2)
+        self.drive(linear=0.0, angular=0.0)
+
+    def drive_forward(self, distance):
+        forward_vel = 0.3
+        self.drive(linear=forward_vel, angular=0.0)
+        sleep(distance / forward_vel)
+        self.drive(linear=0.0, angular=0.0)
+
+
+    def drive(self, linear, angular):
+        msg = Twist()
+        msg.linear.x = linear
+        msg.angular.z = angular
+        self.vel_pub.publish(msg)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
